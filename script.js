d238691475763c21b86ccf54a70ab811aebb23dc
kapcak <simeonbd@gmail.com>
2021-08-05 09:21:57 +0300

    Removed comma by using join

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const goods = [
+    {title: 'Shirt', price: 150},
+    {title: 'Socks', price: 50},
+    {title: 'Jacket', price: 350},
+    {title: 'Shoes', price: 250},
+];
+
+const renderGoodsItem = (title, price) => {
+    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
+};
+
+const renderGoodList = (list) => {
+    let goodsList = list.map(item => renderGoodsItem(item.title, item.price)).join('');
+    document.querySelector('.goods-list').innerHTML = goodsList;
+}
+
+renderGoodList(goods);
