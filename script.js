0be535d8e1a35b7791a3b712013227699b9b27be
kapcak <simeonbd@gmail.com>
2021-08-09 13:15:52 +0300

    Added bucket classes

--- script.js
+++ script.js
@@ -49,6 +49,39 @@ class GoodsList {
 }
 
 
+// Класс элемента корзины расширяет возможности класса товара,
+// теперь у него есть кол-во
+class BucketItem extends GoodsItem{
+    constructor(title, price, quantity) {
+        super(title, price);
+        this.quantity = quantity
+    }
+    render() {
+        return `<div class="goods-item"><h3 class="goods-title">${this.title}</h3><p class="price">${this.price}</p><p class="quantity">${this.quantity}</p></div>`;
+    }
+    position_total() {
+        return this.price * this.quantity
+    }
+}
+
+class BucketList {
+    constructor() {
+        this.goodslist = []
+    };
+    fetchItems(){
+        this.goodslist = [
+            new BucketItem('Shirt', 150, 10),
+            new BucketItem('Socks', 50, 15),
+            new BucketItem('Jacket', 350, 4),
+            new BucketItem('Shoes', 250, 12),
+        ];
+    };
+    render(){};
+    total_price() {
+        return this.goodslist.reduce((acc, cur) => acc + cur.position_total(), 0);
+    }
+}
+
 const list = new GoodsList();
 list.fetchGoods();
 list.render();
