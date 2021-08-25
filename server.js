1953a74cad0709b61a5ac36deb32f622e2d69614
kapcak <simeonbd@gmail.com>
2021-08-25 16:35:40 +0300

    Added ex 1 and 2 from lesson 7

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const app = express();
+const jsonParser = express.json();
+
+app.use(express.static('.'));
+
+app.get('/catalogData', (req, res) => {
+  fs.readFile('catalog.json', 'utf-8', (err, data) => {
+    // console.log(data);
+    res.send(data);
+  });
+});
+
+app.post('/addToCart', jsonParser, (req, res) => {
+  fs.readFile('cart.json', 'utf-8', (err, data) => {
+    if (err) {
+      res.send('{"result": 0}');
+    } else {
+      const cart = JSON.parse(data);
+      const item = req.body;
+      console.log(`Adding ${item.product_name}`);
+      item.id = cart.length;
+      cart.push(item);
+      fs.writeFile('cart.json', JSON.stringify(cart), (error) => {
+        console.log('done');
+      });
+    }
+  });
+});
+
+app.get('/getBasket', (req, res) => {
+  fs.readFile('cart.json', 'utf-8', (err, data) => {
+    res.send(data);
+  });
+});
+
+app.post('/deleteFromCart', jsonParser, (req, res) => {
+  fs.readFile('cart.json', 'utf-8', (err, data) => {
+    if (err) {
+      res.send('{"result": 0}');
+    } else {
+      const cart = JSON.parse(data);
+      if (cart.length === 0) {
+        console.log('Empty cart');
+      } else {
+        const item = req.body;
+        console.log(`Deleting ${item.product_name} id: ${item.id}`);
+        const index = cart.findIndex((i) => i.id === item.id);
+        cart.splice(index, 1);
+        fs.writeFile('cart.json', JSON.stringify(cart), (error) => {
+          console.log('done');
+        });
+      }
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log('server is running on port 3000!');
+});
